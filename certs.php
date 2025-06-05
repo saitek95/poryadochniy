81a1fddbd0881300e4e1d0db4242535137589e25
ilgiz <saitek95@mail.ru>
2025-06-05 22:58:48 +0300

    start

---
+++ certs.php
@@ -0,0 +1,114 @@
+<?php include 'header.php'; ?>
+<div class="max_width_theme breadcrumb">
+    <ul>
+        <li><a href="">Главная</a></li>
+        <img src="assets/svg/arrow_breadcrumb.svg" alt="" class="svg">
+        <li>Каталог</li>
+    </ul>
+</div>
+<div class="max_width_theme certs">
+    <h1 class="page_title">
+        Выберите номинал
+    </h1>
+    <div class="product_certs">
+        <div class="certs_list">
+            <ul>
+                <li>
+                    <a href="">1 000</a>
+                </li>
+                <li>
+                    <a href="">2 000</a>
+                </li>
+                <li>
+                    <a href="" class="active">3 000</a>
+                </li>
+                <li>
+                    <a href="">5 000</a>
+                </li>
+                <li>
+                    <a href="">10 000</a>
+                </li>
+                <li>
+                    <a href="">20 000</a>
+                </li>
+            </ul>
+        </div>
+        <div class="total_sum_certs">
+            <div class="top_block">
+                <p class="name">Итого</p>
+                <p class="sum">3.000 ₽</p>
+            </div>
+            <a href="" class="btn_default">Добавить в корзину</a>
+        </div>
+        <div class="certs_img">
+            <img src="assets/img/Screenshot_3%201.png" alt="">
+        </div>
+    </div>
+    <div class="banner_block">
+        <img src="assets/img/banner_certs_block.png" alt="">
+    </div>
+    <div class="accordion">
+        <ul>
+            <li>
+                <div class="name">
+                    Как работает сертификат?
+                    <img src="assets/svg/accordion_arrow.svg" alt="" class="svg">
+                </div>
+                <div class="content">
+                    <p>Вы выбираете дизайн и номинал, оплачиваете его удобным способом и получаете pdf-файл
+                        с сертификатом. Его можно скачать и отправить в сообщении или распечатать, чтобы вручить лично.</p>
+                    <p>Дальше — дело за получателем: он активирует сертификат в профиле, и деньги приходят в его
+                        Кошелёк. Если кошелька нет, сразу же предложим его создать: это мгновенно и бесплатно</p>
+                </div>
+            </li>
+            <li>
+                <div class="name">
+                    Где хранятся сертификаты после покупки?
+                    <img src="assets/svg/accordion_arrow.svg" alt="" class="svg">
+                </div>
+                <div class="content">
+                    <p>Профиль → Покупки → Все товары → Подарочные сертификаты</p>
+                </div>
+            </li>
+            <li>
+                <div class="name">
+                    На что можно потратить сертификат?
+                    <img src="assets/svg/accordion_arrow.svg" alt="" class="svg">
+                </div>
+                <div class="content">
+                    <p>На любые товары на Порядочный, кроме другого сертификата. Если денег от сертификата не будет
+                        хватать, получатель сможет добавить свои и купить всё, что хочется</p>
+                </div>
+            </li>
+            <li>
+                <div class="name">
+                    Сколько действует сертификат?
+                    <img src="assets/svg/accordion_arrow.svg" alt="" class="svg">
+                </div>
+                <div class="content">
+                    <p>Его нужно активировать в течение 2 лет. После активации деньги хранятся без ограничений.</p>
+                </div>
+            </li>
+            <li>
+                <div class="name">
+                    Можно ли вывести деньги от сертификата?
+                    <img src="assets/svg/accordion_arrow.svg" alt="" class="svg">
+                </div>
+                <div class="content">
+                    <p>Нет, они остаются в Кошельке, пока их не потратить</p>
+                </div>
+            </li>
+            <li>
+                <div class="name">
+                    Как вернуть сертификат?
+                    <img src="assets/svg/accordion_arrow.svg" alt="" class="svg">
+                </div>
+                <div class="content">
+                    <p>Проще всего — его активировать и потратить деньги на покупки на Порядочный. Если вы хотите
+                        вернуть деньги туда, откуда они списались, почитайте полные условия возврата.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
